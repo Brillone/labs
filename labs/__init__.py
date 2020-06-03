43fdb4de2aa5ae23926895f4b3b5a5fb6c8364eb
ebrill <brillone@gmail.com>
2020-06-03 18:36:56 +0300

    fix new sklearn versions skopt incompatibility

--- labs/__init__.py
+++ labs/__init__.py
@@ -1,12 +1,16 @@
-import sklearn.utils.fixes
-from numpy.ma import MaskedArray
-
 # internal
 from .wrappers import experiment_path
 
 
 # version
-__version__ = '0.0.2'
+__version__ = '0.0.3'
+
 
 # fix skopt MaskedArray incompatibility
-sklearn.utils.fixes.MaskedArray = MaskedArray
+try:
+    import sklearn.utils.fixes
+    from numpy.ma import MaskedArray
+
+    sklearn.utils.fixes.MaskedArray = MaskedArray
+except:
+    pass
